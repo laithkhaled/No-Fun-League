277e137bea8d99efe3244fc2a228fb48a9341989
lexiloo731 <lexiloo731@gmail.com>
2024-02-26 18:21:08 -0600

    Delay for stamina
    Player takes time to catch breath when stamina runs out

--- Assets/Scripts/CharaterStamina.cs
+++ Assets/Scripts/CharaterStamina.cs
@@ -18,40 +18,48 @@ public class CharaterStamina : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Reduces stamina bar if player if running
-        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0 && !isOutOfBreath)
+        if (isOutOfBreath != true)
         {
-            CharacterMovement.isRunning = true;
-            stamina -= staminaDcrRate;
-
-            // If stamina reaches 0 then player is out of breath and cannot sprint 
-            // until stamina refills
-            if (stamina <= 0)
+            // Reduces stamina bar if player if running
+            if (Input.GetKey(KeyCode.LeftShift) && stamina > 0 && !isOutOfBreath)
             {
-                isOutOfBreath = true;
+                CharacterMovement.isRunning = true;
+                stamina -= staminaDcrRate;
+
+                // If stamina reaches 0 then player is out of breath and cannot sprint 
+                // until stamina refills
+                if (stamina <= 0)
+                {
+                    isOutOfBreath = true;
+                    CharacterMovement.isRunning = false;
+                }
+            }
+            else
+            {
                 CharacterMovement.isRunning = false;
             }
-        }
-        else
-        {
-            CharacterMovement.isRunning = false;
-        }
-
-        // If out of breath, stop reducing stamina and start refilling
-        if (isOutOfBreath && stamina < totalStamina && !Input.GetKey(KeyCode.LeftShift))
-        {
-            stamina += staminaIncRate;
-        }
-        // Otherwise, passively regenerate stamina
-        else
-        {
-            stamina += staminaIncRate;
         }
 
         // If stamina is refilled, allow the player to run again
         if (stamina >= totalStamina)
         {
             isOutOfBreath = false;
+        }
+
+        // Delay initially when out of breath
+        if (isOutOfBreath == true && stamina == 0)
+        {
+            Invoke("CatchBreath", 1.5f);
+            // Debug.Log("Caught Breath.");
+        }
+        else if (isOutOfBreath && Input.GetKey(KeyCode.LeftShift))
+        {
+            // nothing happens
+        }
+        // Otherwise, passively regenerate stamina
+        else
+        {
+            stamina += staminaIncRate * 0.40f;
         }
 
         // Stamina stays in range of 0 - totalStamina
@@ -61,6 +69,15 @@ public class CharaterStamina : MonoBehaviour
         if (staminaBar != null)
         {
             staminaBar.transform.localScale = new Vector2(stamina / totalStamina, staminaBar.transform.localScale.y);
+        }
+    } 
+
+   void CatchBreath()
+    {
+        // If out of breath, stop reducing stamina and start refilling
+        if (isOutOfBreath && !Input.GetKey(KeyCode.LeftShift))
+        {
+            stamina += staminaIncRate;
         }
     }
 }
